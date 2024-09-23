0eb003f9c206655f45758dfb39dfb979aabdbc88
Jacob Fredriksson <fredriksson.jacob@gmail.com>
2024-09-23 15:01:40 +0200

    asdasd
    asdasd

---
+++ README.md
@@ -0,0 +1,42 @@
+# Netmiko Send Commands
+Script to send commands from a file to a list of devices (routers and switches running IOS).
+The output is saved in a .txt.
+
+# Requires
+- Python (tested on Python 2.7 and 3.6)
+- Modules
+  - Netmiko >= 2.2.2
+  - getpass
+  - csv
+- You can use pip install -r requirements.txt to install all modules
+
+# Supports
+Routers and Switches Cisco running IOS with SSH/Telnet configured (default ports 22/23)
+
+# Limitations
+- Same credentials for all devices
+- Only SSH/Telnet with default ports (22/23)
+- Last line on configuration file can not be 'exit'
+- Only config that can be the same in different devices
+
+# Usage
+1) Install Python and the required modules
+2) Download this repository or copy files commands_to_send.txt, devices_to_configure.csv and send_commands.py to the same folder
+4) Inform port (22/23) and IP address of devices in devices_to_configure.csv file
+![devices](https://user-images.githubusercontent.com/17407109/108557485-71efad00-72d7-11eb-8ca6-33b452f9e621.PNG)
+3) Put the commands that you want to send to devices in file commands_to_send.txt (it starts in config mode)
+![commands](https://user-images.githubusercontent.com/17407109/108557453-64d2be00-72d7-11eb-8c7f-e00e3676a834.PNG)
+4) Run pynton send_commands.py
+5) Inform username, password and enable
+
+# Use case
+To perform backup, to enables logging, to collects information, to creates username, shut/no shut interfaces, to removes config, any other config/command that can be repeated over different devices
+
+# Getting Help
+If you are having trouble or need help, create an issue [here](https://github.com/andreirapuru/netmiko_send_commands/issues)
+
+# Credits and references
+All credits to Kirk Byers for making [Netmiko](https://github.com/ktbyers/netmiko)
+
+
+[![published](https://static.production.devnetcloud.com/codeexchange/assets/images/devnet-published.svg)](https://developer.cisco.com/codeexchange/github/repo/andreirapuru/netmiko_send_commands)
---
+++ send_commands.py
@@ -0,0 +1,225 @@
+# Created by Jacob Fredriksson
+# https://wiresandwi.fi
+# https://github.com/wiresandwifi???????????
+
+import getpass
+import netmiko
+import csv
+from datetime import datetime
+#import colorama
+from netmiko import ConnectHandler
+from netmiko.exceptions import AuthenticationException
+from netmiko.exceptions import NetMikoTimeoutException
+from netmiko.exceptions import SSHException
+from colorama import Fore, Back, Style, init
+import textfsm
+import pprint
+
+#import logging
+#Enable debug (optional)
+#logging.basicConfig(filename='netmiko_logs.txt', level=logging.DEBUG)
+#logger = logging.getLogger("netmiko")
+
+print(Back.YELLOW)
+#print('\n' '.:.:. .:.:. .:.:. .:.:. .:.:.')
+print('\n' ' .:.:. Script initiated  .:.:.')
+#print('\n' '.:.:. .:.:. .:.:. .:.:. .:.:.')
+#print('\n')
+print(Style.RESET_ALL)
+
+# Ask user for device credentials (same credentials userd for all devices)
+USER = input('Username: ')
+PASS = getpass.getpass('Password: ')
+# Comment out "EN" below if Enable password is not needed
+# ENABLE = getpass.getpass('Enable: ')
+print('\n')
+print('CONNECTING TO DEVICES... ')
+print('\n')
+
+
+# Default device template
+device_template = {
+'device_type': 'cisco',
+'ip': '1.2.3.4',
+'username': USER,
+'password': PASS,
+'port':22,
+# Remove comment below below if Enable password needs to be used
+#'secret': ENABLE,
+'blocking_timeout': 4 #Default = 8, if timeout problem increase to 16
+}
+
+# Keep track of current time for timestamping log files
+# Create string variable of current time
+current_time = str(datetime.now().replace(microsecond=0))
+#string_current_time = str(current)
+
+# Variable to keep track of failed connections to devices 
+failed_devices_amount = 0
+# List to keep track of device IP addresses 
+failed_devices_ip = []
+# List to keep track of reason for failure to connect to a device
+failed_devices_reason = []
+
+# Open file with devices information (IP and Port)
+list_of_devices = csv.DictReader(open('devices_to_configure.csv'))
+
+# Prepare device_template with information from CSV
+for row in list_of_devices:
+	if (row['port']) == '23':
+		device_template['device_type'] = 'cisco_ios_telnet'
+		device_template['port'] = '23'
+		device_template['ip'] = row['ip']
+	else:
+		device_template['device_type'] = 'cisco_ios'
+		device_template['port'] = '22'
+		device_template['ip'] = row['ip']
+	try:
+		print ('====== Logging IN to device', device_template['ip'],'   ======')
+		#Connect to device and send show/config
+		net_connect = ConnectHandler(**device_template)
+		net_connect.enable()
+		#interface_data (prev. "output") = net_connect.send_command_timing('show run', last_read=1, read_timeout=3000)
+		print(Fore.GREEN + "====== CONNECTED! ======" + Style.RESET_ALL)
+		interface_data = net_connect.send_command('show interfaces switchport', use_textfsm=True)
+		
+		# Print 
+		pprint.pp(interface_data)
+		print ('====== Running commands on', device_template['ip'],'   ======')
+		#Connect to device and send config
+		#net_connect = ConnectHandler(**device_template)
+		#net_connect.enable()
+
+		for interface in interface_data:
+			if interface['admin_mode'] == 'static access':
+       		# Replace this with the actual command you want to run
+				print(f"Running command(s) on {interface['interface']}")
+				enter_interface = f"interface {interface['interface']}"
+				#print(enter_interface)
+				#print(enter_interface)
+				output1 = net_connect.send_config_set(enter_interface)
+				#output2 = net_connect.send_config_from_file('commands_to_send.txt')
+				with open('commands_to_send.txt', 'r') as file:
+					commands = file.readlines()
+					# Strip any newline characters and send the commands within the interface context
+					commands = [cmd.strip() for cmd in commands]
+				full_command_list = [enter_interface] + commands
+				output2 = net_connect.send_config_set(full_command_list)
+				#output3 = net_connect.send_config_set("do write mem")
+				print(output2)
+
+
+
+		#output = net_connect.send_command_timing('show run', last_read=1, read_timeout=3000)
+		#output = net_connect.send_config_from_file('commands_to_send.txt')
+		#print(Fore.GREEN + "====== CONNECTED! ======" + Style.RESET_ALL)
+		#print(output)
+		#Save logs in file
+		log = open('log_file.txt', 'a')
+		log.write('\n')
+		log.write(current_time)
+		log.write('\n')
+		log.write(device_template['ip'])
+		log.write('\n')
+		log.write(output2)
+		log.write('\n')
+		print ('====== Saving Configuration on device', device_template['ip'],' ======')
+		save_now = net_connect.send_config_set("do write mem")
+		net_connect.disconnect()
+		print ('====== Logging OUT from device', device_template['ip'],' ======')
+		print('\n')
+
+
+
+	# Manage and log Authentication failure to device
+	except AuthenticationException as err1:
+		#print("~" * 15 + str(err2) + "TIMEOUT~" * 15)
+		log = open('log_file.txt', 'a')
+		log.write('\n')
+		log.write(current_time)
+		log.write('\n')
+		log.write('Unable to access the device (Authentication failed) ')
+		log.write(device_template['ip'])
+		log.write('\n')
+		print(Fore.BLACK + Back.RED + '====== Unable to access device', device_template['ip'],' ======' + Style.RESET_ALL)
+		#print(Style.RESET_ALL)
+		# Increase failed_devices variable
+		failed_devices_amount += 1
+		# Add(append) IP address of failed device to list 
+		failed_devices_ip.append(device_template['ip'])
+		# Convert exception error "e" to a string and save in new variable
+		#string_e = str(err1)
+		# Save first line of converted exception error (contains failure reason) as variable
+		#string_e_line1 = string_e.partition('\n')[0]
+		# Add(append) failure reason of failed connection attempt to list
+		#failed_devices_reason.append(string_e_line1)
+		failed_devices_reason.append("Authentication to device failed.")
+
+	# Manage and log Timeout Exception (device unreachable)
+	except NetMikoTimeoutException as err2:
+        #print("~" * 15 + str(err2) + "TIMEOUT~" * 15)
+		log = open('log_file.txt', 'a')
+		log.write('\n')
+		log.write(current_time)
+		log.write('\n')
+		log.write('Unable to access the device (Timeout) ')
+		log.write(device_template['ip'])
+		log.write('\n')
+		print(Fore.BLACK + Back.RED + '====== Unable to access device', device_template['ip'],' ======' + Style.RESET_ALL)
+		#print(Style.RESET_ALL)
+		# Increase failed_devices variable
+		failed_devices_amount += 1
+		# Add(append) IP address of failed device to list 
+		failed_devices_ip.append(device_template['ip'])
+		# Convert exception error "e" to a string and save in new variable
+		#string_e = str(err2)
+		# Save first line of converted exception error (contains failure reason) as variable
+		#string_e_line1 = string_e.partition('\n')[0]
+		# Add(append) failure reason of failed connection attempt to list
+		#failed_devices_reason.append(string_e_line1)
+		failed_devices_reason.append("Connection to device failed.")
+
+		log.close()
+	# Manage and log other Exceptions
+	except SSHException as e:
+		log = open('log_file.txt', 'a')
+		log.write('\n')
+		log.write(current_time)
+		log.write('\n')
+		log.write('Unable to access the device (Unknown Error)')
+		log.write(device_template['ip'])
+		log.write('\n')
+		print(Fore.BLACK + Back.RED + '====== Unable to access device', device_template['ip'],' ======' + Style.RESET_ALL)
+		#print(Style.RESET_ALL)
+		# Increase failed_devices variable
+		failed_devices_amount += 1
+		# Add(append) IP address of failed device to list 
+		failed_devices_ip.append(device_template['ip'])
+		# Convert exception error "e" to a string and save in new variable
+		string_e = str(e)
+		# Save first line of converted exception error (contains failure reason) as variable
+		string_e_line1 = string_e.partition('\n')[0]
+		# Add(append) failure reason of failed connection attempt to list
+		failed_devices_reason.append(string_e_line1)
+
+		log.close()
+
+# If there are failed devices, print the IP address and reason for failure
+if failed_devices_amount > 0:
+	print('\n')
+	print(Fore.BLACK + Back.RED + 'Failed to connect to', failed_devices_amount, 'device(s): ' +  Style.RESET_ALL)
+	# Print IP address and Reason for failed devices
+	for ip, reason in zip(failed_devices_ip, failed_devices_reason):
+			print(ip,'-',reason,)
+else:
+	pass
+
+#print(Style.RESET_ALL)
+print('\n')
+print(Back.GREEN)
+#print('\n' '.:.:. .:.:. .:.:. .:.:. .:.:.')
+print('\n' ' .:.:.  Script finished  .:.:.')
+#print('\n' '.:.:. .:.:. .:.:. .:.:. .:.:.')
+#print('\n')
+print(Style.RESET_ALL)
+print('\n')
